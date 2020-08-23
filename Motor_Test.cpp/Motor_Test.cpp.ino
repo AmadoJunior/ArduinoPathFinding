3ee84b569973f589823507548b38b63b3efe1415
AmadoJunior <amadoelrepalol@gmail.com>
2020-08-23 14:48:53 -0400

    Added voltage check to forward and backward methods, refactored the subArraySum method for efficiency, formatted code and added more comments for reability

--- Motor_Test.cpp/Motor_Test.cpp.ino
+++ Motor_Test.cpp/Motor_Test.cpp.ino
@@ -11,7 +11,6 @@ Servo myServo;
 
 //Servo & UltrasonicS Vars
 long distance;
-int servoAngle = 0;
 int bestPath;
 
 //MOTOR SETUP
@@ -28,100 +27,133 @@ int bestPath;
 //Functions
 
 int findBestPath(UltraSonicDistanceSensor &sensor, Servo &servo){
+  //Initializing
   long distanceCm = 0;
   int servoAngle = 0;
-  servo.write(servoAngle);
   long maxSum = 0;
   long tempSum = 0;
   int count = 0;
   int arr[200];
-  int subArraySize = 24;
+  int subArraySize = 20;
   int pos = 0;
+
+  //Scanning and Setting TempSum
+  servo.write(servoAngle);
   while(servoAngle <= 180){
+    
+    //Docs Recommend 60ms Between Measurements
     distanceCm = sensor.measureDistanceCm();
+    delay(65);
+    
+    //Edge Case: Object Out Of Range
     if(distanceCm == -1){
-      distanceCm = 400;
+      distanceCm = 420;
     }
+    
+    //Filling Temp Array
+    if(servoAngle >= 0 && servoAngle <= subArraySize){
+      tempSum += distanceCm;
+    }
+
+    //Storing and Advancing
     arr[count] = distanceCm;
     count++;
     servoAngle++;
     servo.write(servoAngle);
-    delay(5);
+    
   }
+  
+  //Resetting Head Pos
   servo.write(90);
-  for(int i = 0; i < subArraySize; i++){
-    tempSum += arr[i];
-  }
+
+  //Finding Max SubArray
   for(int i = subArraySize; i < 180; i++){
+    
     tempSum = tempSum - arr[i - subArraySize] + arr[i];
+    
     if(tempSum >= maxSum){
       maxSum = tempSum;
-      pos = i - 12;
+      pos = i - 10;
     }
+    
   }
+  
   return pos;
+  
 }
 
 double turnLeft(double angle, float curVoltagePercentage){
+  
   //Calc Delay Num
-      double percentageL = angle/180;
-      double delayNumL = 500*percentageL;
+    double percentageL = angle/180;
+    double delayNumL = 500*percentageL;
       
-      //Set Directions
-      digitalWrite(R_DIR_1, LOW);
-      digitalWrite(R_DIR_2, HIGH);
-      digitalWrite(L_DIR_1, HIGH);
-      digitalWrite(L_DIR_2, LOW);
+  //Set Directions
+    digitalWrite(R_DIR_1, LOW);
+    digitalWrite(R_DIR_2, HIGH);
+    digitalWrite(L_DIR_1, HIGH);
+    digitalWrite(L_DIR_2, LOW);
 
-      //Calc/Set Speed
-      float speed = 160 / curVoltagePercentage;
-      analogWrite(R_SPEED, speed);
-      analogWrite(L_SPEED, speed);
+  //Calc/Set Speed
+    float speed = 170 / curVoltagePercentage;
+    analogWrite(R_SPEED, speed);
+    analogWrite(L_SPEED, speed);
 
-      //Delay 
-        //delay(30+265);
-      unsigned long startUpCompensation = 30;
-      delay(startUpCompensation + (unsigned long) delayNumL);
-      return delayNumL;
+  //Delay 
+    //delay(30+265);
+     unsigned long startUpCompensation = 30;
+     delay(startUpCompensation + (unsigned long) delayNumL);
+     return delayNumL;
+ 
 }
 
 double turnRight(double angle, float curVoltagePercentage){
+  
   //Calc Delay Num
-      double percentageR = angle/180;
-      double delayNumR = 500*percentageR;
+    double percentageR = angle/180;
+    double delayNumR = 500*percentageR;
 
-      //Set Directions
-      digitalWrite(R_DIR_1, HIGH);
-      digitalWrite(R_DIR_2, LOW);
-      digitalWrite(L_DIR_1, LOW);
-      digitalWrite(L_DIR_2, HIGH);
+  //Set Directions
+    digitalWrite(R_DIR_1, HIGH);
+    digitalWrite(R_DIR_2, LOW);
+    digitalWrite(L_DIR_1, LOW);
+    digitalWrite(L_DIR_2, HIGH);
       
-      //Calc/Set Speed
-      float speed = 160 / curVoltagePercentage;
-      analogWrite(R_SPEED, speed);
-      analogWrite(L_SPEED, speed);
+  //Calc/Set Speed
+    float speed = 170 / curVoltagePercentage;
+    analogWrite(R_SPEED, speed);
+    analogWrite(L_SPEED, speed);
 
-      //Delay
-        //delay(30+265);
-      unsigned long startUpCompensation = 30;
-      delay(startUpCompensation + (unsigned long) delayNumR);
-      return delayNumR;
+  //Delay
+    //delay(30+265);
+    unsigned long startUpCompensation = 30;
+    delay(startUpCompensation + (unsigned long) delayNumR);
+    return delayNumR;
+     
 }
 
-void backUp(){
+void backUp(float curVoltagePercentage){
+  
+  //Setting Direction
   digitalWrite(R_DIR_1, HIGH);
   digitalWrite(R_DIR_2, LOW);
   digitalWrite(L_DIR_1, HIGH);
   digitalWrite(L_DIR_2, LOW);
-  analogWrite(R_SPEED, 160);
-  analogWrite(L_SPEED, 160);
-  delay(250);
+  
+  //Writing Speed
+  float speed = 170 / curVoltagePercentage
+  analogWrite(R_SPEED, (int) speed);
+  analogWrite(L_SPEED, (int) speed);
+  delay(200);
+  
+  //Reseting Speed and Direction
   analogWrite(R_SPEED, 0);
   analogWrite(L_SPEED, 0);
   digitalWrite(R_DIR_1, LOW);
   digitalWrite(R_DIR_2, HIGH);
   digitalWrite(L_DIR_1, LOW);
   digitalWrite(L_DIR_2, HIGH);
+  
 }
 
 void setup() {
@@ -160,22 +192,31 @@ void loop() {
   digitalWrite(L_DIR_2, HIGH);
 
   //Main Logic
-  if(distance > 25 || distance == -1){
-    analogWrite(R_SPEED, 190);
-    analogWrite(L_SPEED, 190);
+  if(distance > 20 || distance == -1){
+    
+    int forwardSpeed = 200 / curVoltagePercentage;
+    analogWrite(R_SPEED, 200);
+    analogWrite(L_SPEED, 200);
+    
   } else {
+    
     analogWrite(R_SPEED, 0);
     analogWrite(L_SPEED, 0);
     delay(100);
-    backUp();
+    backUp(curVoltagePercentage);
     bestPath = findBestPath(sensor, myServo);
+    
     if(bestPath >= 90 && bestPath <= 200){
+      
       double leftAngle = 180 - (double) bestPath;
       leftAngle = 90 - leftAngle;
-      turnLeft(leftAngle+50, curVoltagePercentage);  
+      turnLeft(leftAngle, curVoltagePercentage);  
+      
     } else if(bestPath >= 0 && bestPath < 90){
+      
       double rightAngle = 90 - (double) bestPath;
-      turnRight(rightAngle+50, curVoltagePercentage);  
+      turnRight(rightAngle, curVoltagePercentage);  
+      
     }
   }
 }
